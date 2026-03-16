86dd4272fd6b9519fb1da664f224c0e0975cdd2d
Diego Dominguez-Albiter <fby12@txstate.edu>
2026-03-16 12:30:45 -0500

    SCRUM-70 Created and added Navbar to app

---
+++ app/frontend/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="#" className="logo-container">
+        <img src="coverphoto.jpg" alt="Logo" className="logo-img" />
+        <span className="logo-text">Health Fitness Pro</span>
+      </a>
+
+      <ul className="nav-links">
+        <li><a href="#workout">Workout</a></li>
+        <li><a href="#running">Running</a></li>
+        <li><a href="#nutrition">Nutrition</a></li>
+        <li><a href="#library">Library</a></li>
+      </ul>
+
+      <div className="nav-auth">
+        <a href="login.html" className="btn">LOGIN</a>
+        <a href="signup.html" className="btn">SIGN UP</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
