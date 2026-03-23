f35ed4dc65629f31c762490eff4f3dbb49ffdd9a
Diego Dominguez-Albiter <fby12@txstate.edu>
2026-03-22 23:19:16 -0500

    SCRUM-67 Added Initial React Routing to the React pages in App and main

--- app/frontend/vite-project/src/App.jsx
+++ app/frontend/vite-project/src/App.jsx
@@ -1,24 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import FrontPage from './pages/FrontPage';
 import WorkoutTemplatePage from './pages/WorkoutTemplatePage';
 import WorkoutHistoryPage from './pages/WorkoutHistoryPage';
 import ExerciseLibraryPage from './pages/ExerciseLibraryPage';
 
 function App() {
-  const path = window.location.pathname.toLowerCase();
-
-  if (path === '/workout-template') {
-    return <WorkoutTemplatePage />;
-  }
-
-  if (path === '/workout-history') {
-    return <WorkoutHistoryPage />;
-  }
-
-  if (path === '/exercise-library') {
-    return <ExerciseLibraryPage />;
-  }
-
-  return <FrontPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<FrontPage />} />
+      <Route path="/workout-template" element={<WorkoutTemplatePage />} />
+      <Route path="/workout-history" element={<WorkoutHistoryPage />} />
+      <Route path="/exercise-library" element={<ExerciseLibraryPage />} />
+      <Route path="*" element={<FrontPage />} />
+    </Routes>
+  );
 }
 
 export default App;
