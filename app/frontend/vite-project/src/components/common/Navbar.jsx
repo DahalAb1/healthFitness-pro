db5712cf53fbc1fd317c6d5861701d9b38eeba0e
Saroj Gautam <xey18@txstate.edu>
2026-04-07 15:53:35 -0500

    SCRUM-104: ADDED MOBILE MENU STATE AND CLOSE HANDLER

--- app/frontend/vite-project/src/components/common/Navbar.jsx
+++ app/frontend/vite-project/src/components/common/Navbar.jsx
@@ -1,26 +1,40 @@
-import { Link, NavLink } from 'react-router-dom';
-import coverphoto from '../../assets/coverphoto.jpg';
-import { useAuth } from '../../context/useAuth';
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import coverphoto from "../../assets/coverphoto.jpg";
+import { useAuth } from "../../context/useAuth";
 
 function Navbar() {
   const { user, avatar } = useAuth();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const initials = user
-    ? (user.display_name || user.email?.split('@')[0] || '?').slice(0, 2).toUpperCase()
+    ? (user.display_name || user.email?.split("@")[0] || "?")
+        .slice(0, 2)
+        .toUpperCase()
     : null;
+
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <nav className="navbar">
-      <Link to="/" className="logo-container">
+      <Link to="/" className="logo-container" onClick={closeMenu}>
         <img src={coverphoto} alt="Logo" className="logo-img" />
         <span className="logo-text">Health Fitness Pro</span>
       </Link>
 
       <ul className="nav-links">
-        <li><NavLink to="/workout-template">Templates</NavLink></li>
-        <li><NavLink to="/nutrition">Nutrition</NavLink></li>
-        <li><NavLink to="/exercise-library">Library</NavLink></li>
-        <li><NavLink to="/history">History</NavLink></li>
+        <li>
+          <NavLink to="/workout-template">Templates</NavLink>
+        </li>
+        <li>
+          <NavLink to="/nutrition">Nutrition</NavLink>
+        </li>
+        <li>
+          <NavLink to="/exercise-library">Library</NavLink>
+        </li>
+        <li>
+          <NavLink to="/history">History</NavLink>
+        </li>
       </ul>
 
       <div className="nav-auth">
@@ -30,8 +44,12 @@ function Navbar() {
           </Link>
         ) : (
           <>
-            <Link to="/login" className="btn">LOGIN</Link>
-            <Link to="/signup" className="btn">SIGN UP</Link>
+            <Link to="/login" className="btn">
+              LOGIN
+            </Link>
+            <Link to="/signup" className="btn">
+              SIGN UP
+            </Link>
           </>
         )}
       </div>
