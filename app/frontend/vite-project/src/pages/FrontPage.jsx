f390b06758aad57ab35248834faba413673c09ba
Angel Verde <usn3@txstate.edu>
2026-04-08 15:35:48 -0500

    SCRUM-110 Connected about section to Frontpage

--- app/frontend/vite-project/src/pages/FrontPage.jsx
+++ app/frontend/vite-project/src/pages/FrontPage.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/common/Navbar';
 import HeroSlideshow from '../components/frontpage/HeroSlideshow';
 import MissionSection from '../components/frontpage/MissionSection';
+import AboutSection from '../components/frontpage/AboutSection';
 import TestimonialsSection from '../components/frontpage/TestimonialsSection';
 import FeaturesSection from '../components/frontpage/FeaturesSection';
 import Footer from '../components/common/Footer';
@@ -11,6 +12,7 @@ function FrontPage() {
     <>
       <Navbar />
       <HeroSlideshow />
+      <AboutSection />
       <MissionSection />
       <TestimonialsSection />
       <FeaturesSection />
