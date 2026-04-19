bd9a8067c60cbd873b9cc01976b93e057acd71c3
Diego Dominguez-Albiter <fby12@txstate.edu>
2026-04-19 00:00:43 -0500

    SCRUM-108 Updated History Hero and deleted excess css

--- app/frontend/vite-project/src/components/history/HistoryHero.jsx
+++ app/frontend/vite-project/src/components/history/HistoryHero.jsx
@@ -1,11 +1,12 @@
+import PageHero from '../common/PageHero';
+
 function HistoryHero() {
   return (
-    <div className="feature-hero">
-      <h1 className="history-heading">History</h1>
-      <p className="history-description">
-        Your fitness journey at a glance. Select a day to review specific session data or analyze trends below.
-      </p>
-    </div>
+    <PageHero
+      className="feature-hero"
+      title="History"
+      description="Your fitness journey at a glance. Select a day to review specific session data or analyze trends below."
+    />
   );
 }
 
